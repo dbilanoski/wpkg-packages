893598115c41e62e92c0e86d848970dc8a9bf976
Danilo Bilanoski <dubssta@gmail.com>
2022-11-09 17:24:06 +0100

    Packages revision x7.

---
+++ assets/cisco_ipc-enable-user-priviledges.ps1
@@ -0,0 +1,15 @@
+﻿# Author: Danilo Bilanoski
+# Description: Allow non-administrative users to edit settings in Cisco IP Communicator
+
+$acl = Get-Acl "HKLM:\SOFTWARE\WOW6432Node\Cisco Systems, Inc.\Communicator"
+$person = [System.Security.Principal.NTAccount]"Authenticated Users"         
+$access = [System.Security.AccessControl.RegistryRights]"FullControl"
+$inheritance = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit,ObjectInherit"
+$propagation = [System.Security.AccessControl.PropagationFlags]"None"
+$type = [System.Security.AccessControl.AccessControlType]"Allow"
+
+$rule = New-Object System.Security.AccessControl.RegistryAccessRule($person,$access,$inheritance,$propagation,$type)
+
+$acl.AddAccessRule($rule)
+
+$acl |Set-Acl
