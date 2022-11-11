3140612fb847b5aeb9dfe66188bd5ff2540b04f7
Danilo Bilanoski <dubssta@gmail.com>
2022-11-11 15:37:11 +0100

    Packages revision x8.

---
+++ assets/fonts_install.ps1
@@ -0,0 +1,11 @@
+﻿# Author: Danilo Bilanoski
+# Description: This will install fonts from fon,otf,ttc,ttf collection
+
+$FontFolder = "\\your\fonts-to-install\folder"
+$FontItem = Get-Item -Path $FontFolder
+$FontList = Get-ChildItem -Path "$FontItem\*" -Include ('*.fon','*.otf','*.ttc','*.ttf')
+
+foreach ($Font in $FontList) {
+        Copy-Item $Font "C:\Windows\Fonts"
+        New-ItemProperty -Name $Font.BaseName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $Font.name         
+}
